0391c0a9d28ab42f588ee0336ca4189128975c00
fah-sennalabs <fah@sennalabs.com>
2023-11-13 18:12:12 +0700

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
+    <div className="mt-10 flex flex-col">
+      <div className="mx-auto w-fit flex items-center">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo w-20 h-20" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="logo react w-20 h-20"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-center text-xl font-bold">Vite + React</h1>
+      <div className="flex flex-col items-center space-y-3">
+        <button
+          type="button"
+          className="bg-blue-700 p-2 rounded-md text-white shadow-md"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
+        <div>
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </div>
       </div>
-      <p className="read-the-docs">
+      <p className="text-center read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
